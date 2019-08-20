c842e5b88cfd5703b3cb4390cb3d7dfe3d0089ee
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2019-08-20 14:06:50 +0300

    update for main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles/base.css';
 import './styles/header.css';
 import './styles/products.css';
+import './styles/hello.css';
+import './styles/values.css';
+import './styles/footer.css'
 
 function hello() {
   console.log('hello!');
